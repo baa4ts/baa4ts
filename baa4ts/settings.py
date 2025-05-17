91bb1493dd9834684724825aa28870b2030421ba
Carlos Morales <baa4tsdev@gmail.com>
2025-05-16 22:01:18 -0300

    Update 2

--- baa4ts/settings.py
+++ baa4ts/settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 
 # Archivos estáticos
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 
 # Configuración adicional
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

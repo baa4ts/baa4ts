11cd449f1fd95801a7920d35f2be4c72f0325a24
Carlos Morales <baa4tsdev@gmail.com>
2025-05-16 23:04:19 -0300

    UP

--- baa4ts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # <--- Agrega include aquí
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-]
--- blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import VistaPortfolio
-
-urlpatterns = [
-    path('', VistaPortfolio.as_view(), name='Portafolio'),  # /blog/
-]

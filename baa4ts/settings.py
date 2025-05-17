6e1c6c4477e5807c15076a8419483739f065e912
Carlos Morales <baa4tsdev@gmail.com>
2025-05-16 21:44:55 -0300

    Test 2

--- baa4ts/settings.py
+++ baa4ts/settings.py
@@ -61,13 +61,15 @@ WSGI_APPLICATION = 'baa4ts.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

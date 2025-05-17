7e49271c7e7d1157f01ce4205a2e186012472b07
Carlos Morales <baa4tsdev@gmail.com>
2025-05-16 21:47:27 -0300

    Test 3

--- baa4ts/settings.py
+++ baa4ts/settings.py
@@ -108,11 +108,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Opcionalmente para desarrollo local:
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 

b2cad888c97553dc454f0fadff6c1c16950f23d3
baa4ts <baa4tsdev@gmail.com>
2025-07-19 20:06:45 -0300

    bun -> pnpm

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,9 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from '@tanstack/react-router';
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <main className="bg-black flex flex-col items-center min-h-screen min-w-screen">
       <Outlet />
-    </>
+    </main>
   ),
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router';
-import type { JSX } from 'react';
-import { Titulo } from '../components/index/titulo';
-import { Perfil } from '../components/index/perfil';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 });
 
-function RouteComponent(): JSX.Element {
+// Vista de la ruta /
+function RouteComponent() {
   return (
     <>
-      <main className="bg-black flex flex-col items-center min-h-screen min-w-screen">
-        {/* Titulo de la web: baa4ts */}
-        <Titulo />
-
-        {/* Perfil seccion con la img y la breve descripcion */}
-        <Perfil />
-      </main>
+      <h1>HOla</h1>
     </>
   );
 }

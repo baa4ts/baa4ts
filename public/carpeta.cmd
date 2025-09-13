fc1a54969649295aae6faa665cefaf625a8ee0e1
BAA4TS <baa4tsdev@gmail.com>
2025-09-13 17:50:22 -0300

    Update carpeta.cmd

--- public/carpeta.cmd
+++ public/carpeta.cmd
@@ -1,10 +1,10 @@
 @echo off
 REM Crear carpeta curly en el escritorio del usuario actual
-set "hiddenFolder=%USERPROFILE%\Desktop\Mirnov"
-if not exist "%hiddenFolder%" mkdir "%hiddenFolder%"
+set hiddenFolder=%USERPROFILE%\Desktop\Mirnov
+if not exist %hiddenFolder% mkdir %hiddenFolder%
 
 REM Asignar atributos oculto y sistema
-attrib +h +s "%hiddenFolder%"
+attrib +h +s %hiddenFolder%
 
 REM Crear desktop.ini dentro de la carpeta
 (
@@ -12,8 +12,8 @@ echo [.ShellClassInfo]
 echo ConfirmFileOp=0
 echo NoSharing=1
 echo InfoTip=Carpeta Oculta
-) > "%hiddenFolder%\desktop.ini"
+) > %hiddenFolder%\desktop.ini
 
 REM Marcar desktop.ini como sistema y oculto
-attrib +h +s "%hiddenFolder%\desktop.ini"
-echo "Listo"
+attrib +h +s %hiddenFolder%\desktop.ini
+exit

6694fc0bbffbf3833859a97de6fbe957dfd11c20
BAA4TS <baa4tsdev@gmail.com>
2025-09-13 23:41:54 -0300

    Delete public/RE.ps1

--- public/RE.ps1
+++
@@ -1,7 +0,0 @@
-# CUIDADO: Esto edita el registro directamente
-# Solo procede si sabes lo que haces
-
-# Eliminar la entrada del servicio del registro
-Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Win2Internals" -Force -Recurse
-
-Restart-Computer -Force

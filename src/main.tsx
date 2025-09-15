06cb1006bfd0a9075bb41868d7382b67e2309e11
baa4ts <baa4tsdev@gmail.com>
2025-09-15 16:43:09 -0300

    upda

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
+import ReactDOM from 'react-dom/client'
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Crear el router a partir del árbol de rutas
+const router = createRouter({ routeTree })
+
+// Registrar la instancia del router para seguridad de tipos
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+// Obtener el elemento raíz del HTML
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  
+  // Renderizar la aplicación dentro del StrictMode de React
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  )
+}

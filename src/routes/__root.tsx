4a614ce10feee57cf872e6e61e06300ce0e8a3b3
baa4ts <baa4tsdev@gmail.com>
2025-11-21 17:12:48 -0300

    Update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,29 +11,84 @@ interface MyRouterContext {
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'baa4ts',
-      },
-    ],
-    links: [
-      {
-        rel: 'stylesheet',
-        href: appCss,
-      },
-    ],
-  }),
-  notFoundComponent: () => <div>Página no encontrada 😢</div>,
+  head: () => {
+    //  URL dinamica
+    const currentUrl = typeof window !== 'undefined' ? window.location.href : 'https://baa4ts.is-a-good.dev/';
+
+    return {
+      meta: [
+        {
+          charSet: 'utf-8',
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+        },
+        {
+          title: 'baa4ts',
+        },
+        {
+          name: "description",
+          content: "baa4ts desarrollador backend, tecnologias modernas, soluciones escalables con React, TypeScript, JavaScript y Express.js.",
+        },
+        {
+          property: "og:title",
+          content: "baa4ts",
+        },
+        {
+          property: "og:description",
+          content: "baa4ts desarrollador backend, tecnologias modernas, soluciones escalables con React, TypeScript, JavaScript y Express.js.",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:image",
+          content: "https://i.pinimg.com/1200x/44/c4/83/44c4837d944315f8f4bcb7f9e65e63ad.jpg",
+        },
+        {
+          property: "og:site_name",
+          content: "baa4ts",
+        },
+        {
+          property: "og:url",
+          content: currentUrl,
+        },
+        {
+          name: "twitter:title",
+          content: "baa4ts",
+        },
+        {
+          name: "twitter:description",
+          content: "baa4ts desarrollador backend, tecnologias modernas, soluciones escalables con React, TypeScript, JavaScript y Express.js.",
+        },
+        {
+          name: "twitter:image",
+          content: "https://i.pinimg.com/1200x/44/c4/83/44c4837d944315f8f4bcb7f9e65e63ad.jpg",
+        },
+        {
+          name: "twitter:url",
+          content: currentUrl,
+        },
+        {
+          property: "og:url",
+          content: currentUrl,
+        },
+      ],
+      links: [
+        {
+          rel: 'stylesheet',
+          href: appCss,
+        },
+      ],
+    };
+  },
+  notFoundComponent: () => <div>Pagina no encontrada 😢</div>,
   shellComponent: RootDocument,
 });
+
+
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (

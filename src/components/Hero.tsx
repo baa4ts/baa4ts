0b8fa356744772afc59c8b68d5694b09ea997fec
baa4ts <baa4tsdev@gmail.com>
2026-01-04 09:54:53 -0800

    Update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <article className="md:w-2/5 h-64 md:h-72">
         <img
           className="w-full h-full object-cover"
-          src="https://i.pinimg.com/1200x/44/c4/83/44c4837d944315f8f4bcb7f9e65e63ad.jpg"
+          src="/info.webp"
           alt="baa4ts, desarrollador backend"
           loading="lazy"
         />

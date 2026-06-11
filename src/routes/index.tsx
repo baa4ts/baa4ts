b67ea78dea5711e7b364279e0222805edb59652e
baa4ts <baa4tsdev@gmail.com>
2026-06-11 17:43:04 -0300

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Hero } from '../components/portfolio/Hero'
 import { Skills } from '../components/portfolio/Skills'
-import { Proyectos } from '../components/portfolio/Proyectos'
 
 export const Route = createFileRoute('/')({
     component: RouteComponent,

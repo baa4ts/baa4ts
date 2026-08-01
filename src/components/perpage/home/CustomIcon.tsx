d9c6b3ad019b0e499e900f1072cab8be1022ac0f
Carlos Morales <baa4tsdev@gmail.com>
2026-07-31 23:22:35 -0300

    update

--- src/components/perpage/home/CustomIcon.tsx
+++ src/components/perpage/home/CustomIcon.tsx
@@ -3,27 +3,20 @@ import React from 'react'
 interface Props {
   icono: React.ComponentType<React.SVGProps<SVGSVGElement>>
   color: string
-  animate?: boolean
 }
 
-export const CustomIcon = React.memo(
-  ({ icono: Icono, color, animate = false }: Props) => {
-    return (
-      <>
-        <span
-          className="h-14 w-14 flex items-center justify-center rounded-md border hover:cursor-pointer"
-          style={{
-            backgroundColor: `${color}1a`,
-            borderColor: `${color}40`,
-          }}
-        >
-          <Icono
-            className={animate ? 'animate-pulse' : ''}
-            height="35"
-            width="35"
-          />
-        </span>
-      </>
-    )
-  },
-)
+export const CustomIcon = React.memo(({ icono: Icono, color }: Props) => {
+  return (
+    <>
+      <span
+        className="h-14 w-14 flex items-center justify-center rounded-md border"
+        style={{
+          backgroundColor: `${color}1a`,
+          borderColor: `${color}40`,
+        }}
+      >
+        <Icono height="35" width="35" />
+      </span>
+    </>
+  )
+})
